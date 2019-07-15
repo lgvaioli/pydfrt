44bd5d461a030f907346ddd24e96e0f2c06b16ef
Laureano G. Vaioli <laureano3400@gmail.com>
2019-07-15 16:04:15 -0300

    Improved -p and -pp switches.
    Now the -p and -pp switches are no longer mutually exclusive,
    letting you e.g. rotate all pages within a range except one.

--- pydfrt.py
+++ pydfrt.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3.6
+
 import os
 import sys
 import argparse
@@ -13,10 +15,11 @@ class Command:
     # all_degree: the degree to which all (i.e. even and odd) pages will be
     # rotated. If this value is not zero, the function will ignore even_degree
     # and odd_degree and use all_degree instead.
-    def __init__(self, pdf_in, filename_out, page_range, even_degree, odd_degree, all_degree):
+    def __init__(self, pdf_in, filename_out, single_page, page_range, even_degree, odd_degree, all_degree):
         self.filename_out = filename_out
         self.pdf_reader = PyPDF2.PdfFileReader(pdf_in)
         self.pdf_writer = PyPDF2.PdfFileWriter()
+        self.single_page = single_page
         self.page_range = page_range
         self.even_degree = even_degree
         self.odd_degree = odd_degree
@@ -43,31 +46,48 @@ class Command:
             self.pdf_writer.addPage(page)
             i += 1
         
-        # Now we rotate whatever we need to rotate
-        while(self.page_range[0] <= self.page_range[1]):
-            page = self.pdf_reader.getPage(self.page_range[0])
+        # If -e, -o, or -a were passed, rotate accordingly
+        if(self.all_degree != 0 or self.even_degree != 0 or self.odd_degree != 0):
 
-            # all_degree takes precedence over even_ and odd_degree
-            if self.all_degree > 0:
-                page.rotateClockwise(self.all_degree)
+            while(self.page_range[0] <= self.page_range[1]):
+
+                # Skip single page
+                if(self.page_range[0] == self.single_page[0]):
+                    self.page_range[0] += 1
+                    continue
+
+                page = self.pdf_reader.getPage(self.page_range[0])
+
+                # all_degree takes precedence over even_ and odd_degree
+                if self.all_degree > 0:
+                    page.rotateClockwise(self.all_degree)
+                
+                if self.all_degree < 0:
+                    page.rotateCounterClockwise(self.all_degree)
+
+                # all_degree wasn't set
+                if self.all_degree == 0:
+                    if not (self.page_range[0] + 1) % 2: # page is even
+                        if(self.even_degree > 0):
+                            page.rotateClockwise(self.even_degree)
+                        elif(self.even_degree < 0):
+                            page.rotateCounterClockwise(abs(self.even_degree))
+                    else: # page is odd
+                        if(self.odd_degree > 0):
+                            page.rotateClockwise(self.odd_degree)
+                        elif(self.odd_degree < 0):
+                            page.rotateCounterClockwise(abs(self.odd_degree))
+
+                self.page_range[0] += 1
+
+        # If a single page was passed, rotate it
+        if self.single_page[0] != -1:
+            page = self.pdf_reader.getPage(self.single_page[0])
             
-            if self.all_degree < 0:
-                page.rotateCounterClockwise(self.all_degree)
-
-            # all_degree wasn't set
-            if self.all_degree == 0:
-                if not (self.page_range[0] + 1) % 2: # page is even
-                    if(self.even_degree > 0):
-                        page.rotateClockwise(self.even_degree)
-                    elif(self.even_degree < 0):
-                        page.rotateCounterClockwise(abs(self.even_degree))
-                else: # page is odd
-                    if(self.odd_degree > 0):
-                        page.rotateClockwise(self.odd_degree)
-                    elif(self.odd_degree < 0):
-                        page.rotateCounterClockwise(abs(self.odd_degree))
-
-            self.page_range[0] += 1
+            if self.single_page[0] > 0:
+                page.rotateClockwise(self.single_page[1])
+            elif self.single_page[0] < 0:
+                page.rotateCounterclockwise(abs(self.single_page[1]))
 
         # Write changes to output file and close it 
         self.pdf_writer.write(pdf_out)
@@ -78,6 +98,7 @@ class Command:
     def debug_dump(self):
             print("[DEBUG] Printing Command object")
             print("  Output filename: " + self.filename_out)
+            print("  Single page: " + str(self.single_page[0]) + ", " + str(self.single_page[1]) + " (DEG)")
             print("  Page range: " + str(self.page_range[0]) + "-" + str(self.page_range[1]))
             print("  Even degree: " + str(self.even_degree))
             print("  Odd degree: " + str(self.odd_degree))
@@ -85,16 +106,18 @@ class Command:
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="Pydfrt: A Python PDF rotator. Positive degrees (DEG) mean "
-        "clockwise rotation; negative degrees mean counter-clockwise rotation. Degrees must be a multiple of "
-        "90. Pages are counted starting from 1. In addition to a file, you must specify at least one of "
-        "the following switches: -e, -o, -a. If the -a switch is specified, -e and -o are ignored. By default, "
-        "all pages will be rotated, unless you specify a page range with -pp, or a single page with -p.")
+        "clockwise rotation; negative degrees mean counter-clockwise rotation. A degree of 0 is valid and means "
+        "no rotation. Degrees must be a multiple of 90. Pages are counted starting from 1. In addition to a file, "
+        "you must pass at least one of the following switches: -e, -o, -a, -p. If the -a switch is passed, -e and "
+        "-o are ignored. By default, all pages will be rotated, unless you pass a page range with -pp, or a single "
+        "page with -p. If you pass both -p and -pp, then all pages in the range passed by -pp will be rotated by the "
+        "amount passed by -e, -o, or -a, while the page passed by -p will be rotated by the amount passed by -p. "
+        "Note that if you pass both -p and -pp and -p falls within the -pp range, the -p DEG will overwrite the -pp DEG.")
 parser.add_argument("file", help="input filename")
 parser.add_argument("-fo", "--fileout", metavar="NAME", help="output filename. By default, '_rotated' is appended "
-        "to the input filename. If you specify an output filename and forget to add '.pdf', the program adds it for you")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-p", "--page", metavar="N", help="single page to be rotated", type=int)
-group.add_argument("-pp", "--pagerange", metavar="N", help="range of pages to be rotated", nargs=2, type=int)
+        "to the input filename. If you pass an output filename and forget to add '.pdf', the program adds it for you")
+parser.add_argument("-p", "--page", metavar=("PAGE", "DEG"), help="single page to be rotated", nargs=2, type=int)
+parser.add_argument("-pp", "--pagerange", metavar=("FIRST_PAGE", "LAST_PAGE"), help="range of pages to be rotated", nargs=2, type=int)
 parser.add_argument("-e", "--even", metavar="DEG", help="rotate even pages", type=int)
 parser.add_argument("-o", "--odd", metavar="DEG", help="rotate odd pages", type=int)
 parser.add_argument("-a","--all", metavar="DEG", help="rotate all (i.e. even and odd) pages", type=int)
@@ -113,33 +136,9 @@ except FileNotFoundError as err:
 pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 num_pages = pdf_reader.getNumPages()
 
-# Check and set page range 
-if(args.pagerange):
-
-    if(args.pagerange[0] <= 0):
-        print("Error: pages start at 1. Aborting")
-        sys.exit(-1)
-
-    if(args.pagerange[1] < args.pagerange[0] or args.pagerange[1] > num_pages):
-        print("Error: invalid page range. Aborting")
-        sys.exit(-1)
-
-    page_range = [args.pagerange[0] - 1, args.pagerange[1] - 1]
-
-elif(args.page):
-
-    if(args.page <= 0 or args.page > num_pages):
-        print("Error: invalid page. Aborting")
-        sys.exit(-1)
-    else:
-        page_range = [args.page-1, args.page-1]
-
-else: # neither a range nor a single page was set: set range to all pages
-    page_range = [0, num_pages - 1]
-
 # Check and set rotation degrees
-if not (args.even or args.odd or args.all):
-    print("Error: at least one of the following switches must be specified: -e, -o, -a. Aborting")
+if not (args.even or args.odd or args.all or args.page):
+    print("Error: at least one of the following switches must be passed: -e, -o, -a, -p. Aborting")
     sys.exit(-1)
 
 if(args.even):
@@ -163,15 +162,44 @@ if(args.all):
 else:
     args.all = 0
 
+# Check and set page range and single page
+if(args.pagerange):
+
+    if(args.pagerange[0] <= 0):
+        print("Error: pages start at 1. Aborting")
+        sys.exit(-1)
+
+    if(args.pagerange[1] < args.pagerange[0] or args.pagerange[1] > num_pages):
+        print("Error: invalid page range. Aborting")
+        sys.exit(-1)
+
+    page_range = [args.pagerange[0] - 1, args.pagerange[1] - 1]
+else:
+    page_range = [0, num_pages - 1]
+
+if(args.page):
+
+    if(args.page[0] <= 0 or args.page[0] > num_pages):
+        print("Error: invalid page. Aborting")
+        sys.exit(-1)
+
+    if(args.page[1] % 90 != 0):
+        print("Error: page degree must be a multiple of 90. Aborting")
+        sys.exit(-1)
+
+    single_page = [args.page[0] - 1, args.page[1]]
+else:
+    single_page = [-1, 0]
+
 # Check and set output filename
 if not(args.fileout):
     args.fileout = os.path.splitext(args.file)[0] + "_rotated.pdf"
 elif(os.path.splitext(args.fileout) != ".pdf"):
     args.fileout += ".pdf"
 
-
 # Construct and execute command
-command = Command(pdf_file, args.fileout, page_range, args.even, args.odd, args.all)
+command = Command(pdf_file, args.fileout, single_page, page_range, args.even, args.odd, args.all)
+command.debug_dump()
 command.execute()
 
 # Close input file
